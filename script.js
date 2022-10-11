6bbbbce1d5ff89841c81f53f0b0515491088c33b
Ryan Saucier <ryanyoubum@gmail.com>
2022-10-11 12:48:09 -0400

    reset button added

--- script.js
+++ script.js
@@ -6,12 +6,14 @@ const multiply = document.getElementById("multiply");
 const divide = document.getElementById("divide");
 const numbers = document.querySelectorAll(".number");
 const buttons = document.querySelectorAll(".button");
+const restarts = document.getElementById("reset");
 let input1 = [];
 let input2 = [];
 let turn = input1;
 let screen = document.getElementById("screen");
 let moreData = [];
-let toStr = 0;
+let answer = 0;
+let symbolText = "";
 function numberClick(event) {
   const num = event.target;
   const numClicked = num.innerText;
@@ -29,41 +31,33 @@ function numberClick(event) {
     attach = input2.push(numClicked);
     combined = input2.join("");
     screen.innerText = combined;
-    console.log("input 3: " + combined);
+    console.log("input 2: " + combined);
   }
 }
 
 function symbolClick(event) {
   const symbol = event.target;
   const symbolClicked = symbol.innerText;
-  if (symbolClicked === multiply.innerText && moreData === toStr) {
+  if (symbolClicked === multiply.innerText && moreData === answer) {
     symbolText = "**";
     console.log(symbolText);
     turn = moreData;
     input2.splice(0, input2.length);
-    console.log("input 3: " + input2);
-    console.log(moreData);
-  } else if (symbolClicked === divide.innerText && moreData === toStr) {
+  } else if (symbolClicked === divide.innerText && moreData === answer) {
     symbolText = "//";
     console.log(symbolText);
     turn = moreData;
     input2.splice(0, input2.length);
-    console.log("input 3: " + input2);
-    console.log(moreData);
-  } else if (symbolClicked === minus.innerText && moreData === toStr) {
+  } else if (symbolClicked === minus.innerText && moreData === answer) {
     symbolText = "--";
     console.log(symbolText);
     turn = moreData;
     input2.splice(0, input2.length);
-    console.log("input 3: " + input2);
-    console.log(moreData);
-  } else if (symbolClicked === plus.innerText && moreData === toStr) {
+  } else if (symbolClicked === plus.innerText && moreData === answer) {
     symbolText = "++";
     console.log(symbolText);
     turn = moreData;
     input2.splice(0, input2.length);
-    console.log("input 3: " + input2);
-    console.log(moreData);
   } else if (symbolClicked === multiply.innerText) {
     symbolText = "*";
     console.log(symbolText);
@@ -81,61 +75,71 @@ function symbolClick(event) {
     console.log(symbolText);
     turn = input2;
   }
-  console.log(moreData);
 }
 
 function equalsClick() {
   if (symbolText === "*") {
-    toStr = input1.join("") * input2.join("");
-    screen.innerText = toStr;
-    console.log("output: " + toStr);
-    moreData = toStr;
+    answer = input1.join("") * input2.join("");
+    screen.innerText = answer;
+    console.log("output: " + answer);
+    moreData = answer;
   } else if (symbolText === "**") {
-    toStr = moreData * input2.join("");
-    screen.innerText = toStr;
-    console.log("output: " + toStr);
-    moreData = toStr;
+    answer = moreData * input2.join("");
+    screen.innerText = answer;
+    console.log("output: " + answer);
+    moreData = answer;
   } else if (symbolText === "/") {
-    toStr = input1.join("") / input2.join("");
-    screen.innerText = toStr;
-    console.log("output: " + toStr);
-    moreData = toStr;
+    answer = input1.join("") / input2.join("");
+    screen.innerText = answer;
+    console.log("output: " + answer);
+    moreData = answer;
   } else if (symbolText === "//") {
-    toStr = moreData / input2.join("");
-    screen.innerText = toStr;
-    console.log("output: " + toStr);
-    moreData = toStr;
+    answer = moreData / input2.join("");
+    screen.innerText = answer;
+    console.log("output: " + answer);
+    moreData = answer;
   } else if (symbolText === "+") {
     input1add = Number(input1.join(""));
     input2add = Number(input2.join(""));
-    toStr = input1add + input2add;
-    screen.innerText = toStr;
-    console.log("output: " + toStr);
-    moreData = toStr;
+    answer = input1add + input2add;
+    screen.innerText = answer;
+    console.log("output: " + answer);
+    moreData = answer;
   } else if (symbolText === "++") {
     input1add = Number(input1.join(""));
     input2add = Number(input2.join(""));
-    toStr = moreData + input2add;
-    screen.innerText = toStr;
-    console.log("output: " + toStr);
-    moreData = toStr;
+    answer = moreData + input2add;
+    screen.innerText = answer;
+    console.log("output: " + answer);
+    moreData = answer;
   } else if (symbolText === "-") {
     input1Minus = Number(input1.join(""));
     input2Minus = Number(input2.join(""));
-    toStr = input1Minus - input2Minus;
-    screen.innerText = toStr;
-    console.log("output: " + toStr);
-    moreData = toStr;
+    answer = input1Minus - input2Minus;
+    screen.innerText = answer;
+    console.log("output: " + answer);
+    moreData = answer;
   } else if (symbolText === "--") {
     input1add = Number(input1.join(""));
     input2add = Number(input2.join(""));
-    toStr = moreData - input2add;
-    screen.innerText = toStr;
-    console.log("output: " + toStr);
-    moreData = toStr;
+    answer = moreData - input2add;
+    screen.innerText = answer;
+    console.log("output: " + answer);
+    moreData = answer;
+  } else if (symbolText === "") {
+    restartClick();
   }
 }
 
+function restartClick() {
+  input1.splice(0, input1.length);
+  input2.splice(0, input2.length);
+  moreData = [];
+  screen.innerText = "0";
+  turn = input1;
+}
+
+restarts.addEventListener("click", restartClick);
 numbers.forEach((num) => num.addEventListener("click", numberClick));
 buttons.forEach((symbol) => symbol.addEventListener("click", symbolClick));
 equals.forEach((equal) => equal.addEventListener("click", equalsClick));
